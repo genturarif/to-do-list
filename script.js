16d93706233c1a538be9ac6f3c1b4157d4e657f4
genturarif <gentur1608@gmail.com>
2021-08-16 20:11:33 +0700

    update javascript

--- script.js
+++ script.js
@@ -2,3 +2,37 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  // Prevent Form From Submitting
+  event.preventDefault();
+
+  // Crate Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // Create Todo Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  // Create Mark Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "<i class= 'fas fa-check'></i>";
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // Create Trash Button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = "<i class= 'fas fa-trash'></i>";
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  // Append To todoList
+  todoList.appendChild(todoDiv);
+}
