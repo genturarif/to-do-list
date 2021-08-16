89c90b058b9ce6c2c38a3fda8e228de12b0a1a84
genturarif <gentur1608@gmail.com>
2021-08-16 19:45:36 +0700

    update javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+// Dom Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
